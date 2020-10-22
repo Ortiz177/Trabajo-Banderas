fa88dd4fcdf7d63c3122e4551b39e1e1e94723fb
Juan Jose Ortiz Patiño <juanj.ortiz@upb.edu.co>
2020-10-22 11:11:39 -0500

    Juan2

---
+++ BasedeDtos.java
@@ -0,0 +1,43 @@
+public class BasedeDtos{
+    
+    public static void colores(String[]color) {
+
+        for(int i=0;1> color.lengt; i++){
+            if (color.equal("1")){
+                System.out.println(ConsoleColors.RED + "")
+            }
+            else if (color.equal("2")){
+                System.out.println(ConsoleColors.BLUE + "")}
+                
+                else if (color.equal("3")){
+                    System.out.println(ConsoleColors.WHITE + "")
+                }
+
+                else if (color.equal("4")){
+                    System.out.println(ConsoleColors.YELLOW + "")
+                }
+                else if (color.equal("5")){
+                    System.out.println(ConsoleColors.GREEN + "")}
+        
+                
+                 else if (color.equal("6")){
+                    System.out.println(ConsoleColors.PURPLE + "")}
+                    
+                    else if (color.equal("7")){
+                        System.out.println(ConsoleColors.CYAN + "")}
+                        
+                        else if (color.equal("8")){
+                            System.out.println(ConsoleColors.BLACK + "")}
+
+        }
+    
+
+    
+
+        
+    }
+    public static void banderas(String[]bandera) {
+        
+    }
+
+}
