e48b8e792d4aa9d7c10e8aae29368a828a8b2401
Juan Jose Ortiz Patiño <juanj.ortiz@upb.edu.co>
2020-10-22 11:41:52 -0500

    Juan4

--- BasedeDtos.java
+++ BasedeDtos.java
@@ -37,7 +37,15 @@ public class BasedeDtos{
         
     }
     public static void banderas(String[]bandera) {
+    public static void banderas(String[]bandera,int indice) {
+        for(int i=indice+1; i < inidice + 20; i++){
+
+            fila(bandera[i].slit(";"));
+            System.out.println();
+        }
         
+    }
+
     }
 
 }
