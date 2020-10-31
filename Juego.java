720326efd201d763d631f9d2ef759a9fb5f42916
Juan Jose Ortiz Patiño <juanj.ortiz@upb.edu.co>
2020-10-30 20:54:35 -0500

    ordenar y organizar el codigo segun la guia de estilo JAVA de google

--- Juego.java
+++ Juego.java
@@ -1,6 +1,47 @@
-import java.util.Scanner;
-import java.util.Arrays;
-public class Juego{
+import java.util.*;
+
+public class Juego {
+	public static void main(String[] args) {
+		int score = 0;
+		menu(score);
+	}
+
+	public static int menu(int score) {
+		int centinela = 0;
+
+		do {
+			System.out.println();
+			System.out.println(ConsoleColors.CYAN + " _______  _        _______  _______  _______        _______  _______  _______  _______ ");
+			System.out.println(ConsoleColors.CYAN + "(  ____ \\( \\      (  ___  )(  ____ \\(  ____ \\      (  ____ \\(  ___  )(       )(  ____ \\");
+			System.out.println(ConsoleColors.CYAN + "| (    \\/| (      | (   ) || (    \\/| (    \\/      | (    \\/| (   ) || () () || (    \\/");
+			System.out.println(ConsoleColors.CYAN + "| (__    | |      | (___) || |      | (_____       | |      | (___) || || || || (__    ");
+			System.out.println(ConsoleColors.CYAN + "|  __)   | |      |  ___  || | ____ (_____  )      | | ____ |  ___  || |(_)| ||  __)   ");
+			System.out.println(ConsoleColors.CYAN + "| (      | |      | (   ) || | \\_  )      ) |      | | \\_  )| (   ) || |   | || (      ");
+			System.out.println(ConsoleColors.CYAN + "| )      | (____/\\| )   ( || (___) |/\\____) |      | (___) || )   ( || )   ( || (____/\\");
+			System.out.println(ConsoleColors.CYAN + "|/       (_______/|/     \\|(_______)\\_______)      (_______)|/     \\||/     \\|(_______/");
+			System.out.println("Elige una opcion");
+			System.out.println("1.Jugar");
+			System.out.println("2.Como Jugar");
+			System.out.println("3.Creditos");
+			System.out.println("4.Salir");
+			centinela= ConsoleInput.getInt();
+
+			switch (centinela) {
+				case 1: score += jugar();
+				        break;
+				case 2: comojugar();
+				        break;
+				case 3: creditos();
+								break;
+				case 4: System.out.println("Gracias por jugar! tu puntaje fue: " + score);
+				        break;
+				default: System.out.println("No se encuentra esta opcion");
+								break;
+			}
+		} while (centinela != 4);
+
+		return score;
+  }
 
 	public static int jugar() {
 		String banderas[] = ConsoleFile.read("recursos/info_banderas.csv");
@@ -16,75 +57,34 @@ public class Juego{
 		return pointsCount(validateAnswer(answer, banderas, indices[20]));
 	}
 
-		public static int pointsCount(boolean validation) {
-		int points = 0;
+	public static int[] crearIndices(int total) {
+		int indices[] = new int[total];
+		int contador = 0;
 
-		if (validation == true) {
-			points = 1;
+		for (int i = 0; i < indices.length; i++) {
+			indices[i] = contador;
+			contador += 20;
 		}
-		return points;
+
+		return indices;
 	}
 
-	public static boolean validateAnswer(String answer, String banderas[], int indice) {
-		boolean isMatch = false;
-		String fila[];
-		String country;
+	public static int[] RandomizeArray(int[] array) {
+		Random rgen = new Random();
 
-		fila = banderas[indice].split(";");
-		country = fila[0];
-		System.out.println("Nombre pais: " +country);
-
-		if (country.equals(answer)) {
-			isMatch = true;
+		for (int i = 0; i < array.length; i++) {
+			int randomPosition = rgen.nextInt(array.length);
+			int temp = array[i];
+			array[i] = array[randomPosition];
+			array[randomPosition] = temp;
 		}
-		return isMatch;
+		return array;
 	}
 
-
-	public static void comojugar(){
-		do{
-			System.out.println(ConsoleColors.YELLOW + "   ____                                      _                                ");
-			System.out.println(ConsoleColors.YELLOW + "  / ___|    ___    _ __ ___     ___         | |  _   _    __ _    __ _   _ __ ");
-			System.out.println(ConsoleColors.GREEN +  "  | |      / _ \\  | '_ ` _ \\   / _ \\     _  | | | | | |  / _` |  / _` | | '__|");
-			System.out.println(ConsoleColors.GREEN +  "  | |___  | (_) | | | | | | | | (_) |   | |_| | | |_| | | (_| | | (_| | | |   ");
-			System.out.println(ConsoleColors.PURPLE + "   \\____|  \\___/  |_| |_| |_|  \\___/     \\___/   \\__,_|  \\__, |  \\__,_| |_|   ");
-			System.out.println(ConsoleColors.PURPLE + "                                                        |___/                ");
-			System.out.println("1. Elige la opcion jugar");
-            System.out.println("2. Leer las preguntas y contestarlas");
-            System.out.println("3.Acomula puntos");
-	    }
-	}
-
-	public static void creditos(){
-        System.out.println();
-        System.out.println(ConsoleColors.YELLOW + "..######..########..########.########..####.########..#######...######........###.....##.");
-        System.out.println(ConsoleColors.YELLOW + ".##....##.##.....##.##.......##.....##..##.....##....##.....##.##....##......##.##...####");
-        System.out.println(ConsoleColors.YELLOW + ".##.......##.....##.##.......##.....##..##.....##....##.....##.##...........##...##...##.");
-        System.out.println(ConsoleColors.YELLOW + ".##.......########..######...##.....##..##.....##....##.....##..######.....##.....##.....");
-        System.out.println(ConsoleColors.YELLOW + ".##.......##...##...##.......##.....##..##.....##....##.....##.......##....#########..##.");
-        System.out.println(ConsoleColors.YELLOW + ".##....##.##....##..##.......##.....##..##.....##....##.....##.##....##....##.....##.####");
-        System.out.println(ConsoleColors.YELLOW + "..######..##.....##.########.########..####....##.....#######...######.....##.....##..##.");
-        System.out.println();
-        System.out.println();
-		System.out.println(ConsoleColors.PURPLE + ".......##.##.....##....###....##....##..........##..#######...######..########");
-		System.out.println(ConsoleColors.PURPLE + ".......##.##.....##...##.##...###...##..........##.##.....##.##....##.##......");
-		System.out.println(ConsoleColors.PURPLE + ".......##.##.....##..##...##..####..##..........##.##.....##.##.......##......");
-		System.out.println(ConsoleColors.PURPLE + ".......##.##.....##.##.....##.##.##.##..........##.##.....##..######..######..");
-		System.out.println(ConsoleColors.PURPLE + ".##....##.##.....##.#########.##..####....##....##.##.....##.......##.##......");
-		System.out.println(ConsoleColors.PURPLE + ".##....##.##.....##.##.....##.##...###....##....##.##.....##.##....##.##......");
-		System.out.println(ConsoleColors.PURPLE + "..######...#######..##.....##.##....##.....######...#######...######..########");
-		System.out.println();
-		System.out.println();
-		System.out.println(ConsoleColors.BLUE + "....###....##.......########.......##....###....##....##.########..########...#######.");
-		System.out.println(ConsoleColors.BLUE + "...##.##...##.......##.............##...##.##...###...##.##.....##.##.....##.##.....##");
-		System.out.println(ConsoleColors.BLUE + "..##...##..##.......##.............##..##...##..####..##.##.....##.##.....##.##.....##");
-		System.out.println(ConsoleColors.BLUE + ".##.....##.##.......######.........##.##.....##.##.##.##.##.....##.########..##.....##");
-        System.out.println(ConsoleColors.BLUE + ".#########.##.......##.......##....##.#########.##..####.##.....##.##...##...##.....##");
-        System.out.println(ConsoleColors.BLUE + ".##.....##.##.......##.......##....##.##.....##.##...###.##.....##.##....##..##.....##");
-        System.out.println(ConsoleColors.BLUE + ".##.....##.########.########..######..##.....##.##....##.########..##.....##..#######.");
-        System.out.println();
-        System.out.println();
-		System.out.println(ConsoleColors.YELLOW + "Somos unos programadores principiantes, ojala disfruten del juego ;)");
+	public static void imprimirBandera(String[] banderas, int indice) {
+		for (int i = indice; i < indice + 20; i++) {
+			colores(banderas[i].split(";"));
+		}
 	}
 
 	public static void colores(String fila[]) {
@@ -118,77 +118,72 @@ public class Juego{
 		System.out.println(ConsoleColors.RESET);
 	}
 
-	                       
-	public static void imprimirBandera(String[] banderas, int indice) {
-		for (int i = indice; i < indice + 20; i++) {
-			colores(banderas[i].split(";"));
+	public static boolean validateAnswer(String answer, String banderas[], int indice) {
+		boolean isMatch = false;
+		String fila[];
+		String country;
+
+		fila = banderas[indice].split(";");
+		country = fila[0];
+		System.out.println("Nombre pais: " +country);
+
+		if (country.equals(answer)) {
+			isMatch = true;
 		}
+		return isMatch;
 	}
 
-	public static int[] RandomizeArray(int[] array){
-		Random rgen = new Random();
-		for (int i=0;i<array.length;i++ ) {
-			int randomPosition = rgen.nextInt(array.length);
-			int temp = array[i];
-			array[i]=array[randomPosition];
-			array[randomPosition] = temp;
+
+	public static int pointsCount(boolean validation) {
+		int points = 0;
+
+		if (validation == true) {
+			points = 1;
 		}
-		return array;
+		return points;
 	}
 
-
-
-	public static int[] crearIndices(int total){
-		int indices[]= new int[total];
-		int contador = 0;
-		for (int i=0;i<indices.length;i++ ) {
-			indices[i] = contador;
-			contador+=20;
-			
-		}
-		return indices;
+	public static void comojugar() {
+		System.out.println(ConsoleColors.YELLOW + "   ____                                      _                                ");
+    System.out.println(ConsoleColors.YELLOW + "  / ___|    ___    _ __ ___     ___         | |  _   _    __ _    __ _   _ __ ");
+    System.out.println(ConsoleColors.GREEN +  "  | |      / _ \\  | '_ ` _ \\   / _ \\     _  | | | | | |  / _` |  / _` | | '__|");
+    System.out.println(ConsoleColors.GREEN +  "  | |___  | (_) | | | | | | | | (_) |   | |_| | | |_| | | (_| | | (_| | | |   ");
+    System.out.println(ConsoleColors.PURPLE + "   \\____|  \\___/  |_| |_| |_|  \\___/     \\___/   \\__,_|  \\__, |  \\__,_| |_|   ");
+    System.out.println(ConsoleColors.PURPLE + "                                                        |___/                ");
+    System.out.println("1. Elige la opcion jugar");
+    System.out.println("2. Leer las preguntas y contestarlas");
+    System.out.println("3. Acomula puntos");
 	}
 
-	public static void menu(){
-
-		int centinela = 0;
-
-		do {
-			System.out.println();
-			System.out.println(ConsoleColors.CYAN + " _______  _        _______  _______  _______        _______  _______  _______  _______ ");
-			System.out.println(ConsoleColors.CYAN + "(  ____ \\( \\      (  ___  )(  ____ \\(  ____ \\      (  ____ \\(  ___  )(       )(  ____ \\");
-			System.out.println(ConsoleColors.CYAN + "| (    \\/| (      | (   ) || (    \\/| (    \\/      | (    \\/| (   ) || () () || (    \\/");
-			System.out.println(ConsoleColors.CYAN + "| (__    | |      | (___) || |      | (_____       | |      | (___) || || || || (__    ");
-			System.out.println(ConsoleColors.CYAN + "|  __)   | |      |  ___  || | ____ (_____  )      | | ____ |  ___  || |(_)| ||  __)   ");
-			System.out.println(ConsoleColors.CYAN + "| (      | |      | (   ) || | \\_  )      ) |      | | \\_  )| (   ) || |   | || (      ");
-			System.out.println(ConsoleColors.CYAN + "| )      | (____/\\| )   ( || (___) |/\\____) |      | (___) || )   ( || )   ( || (____/\\");
-			System.out.println(ConsoleColors.CYAN + "|/       (_______/|/     \\|(_______)\\_______)      (_______)|/     \\||/     \\|(_______/");
-			System.out.println("Elige una opcion");
-			System.out.println("1.Jugar");
-			System.out.println("2.Como Jugar");
-			System.out.println("3.Creditos");
-			System.out.println("4.Salir");
-			centinela= ConsoleInput.getInt();
-
-			switch (centinela) {
-				case 1: score += jugar();
-				        break;
-				case 2: comojugar();
-				        break;
-				case 3: creditos();
-						break;
-				case 4: System.out.println("Gracias por jugar! tu puntaje fue: " + score);
-				        break;
-				default: System.out.println("No se encuentra esta opcion");
-						break;
-			}
-		} while (centinela != 4);
-
-		return score;
+		public static void creditos(){
+    System.out.println();
+    System.out.println(ConsoleColors.YELLOW + "..######..########..########.########..####.########..#######...######........###.....##.");
+    System.out.println(ConsoleColors.YELLOW + ".##....##.##.....##.##.......##.....##..##.....##....##.....##.##....##......##.##...####");
+    System.out.println(ConsoleColors.YELLOW + ".##.......##.....##.##.......##.....##..##.....##....##.....##.##...........##...##...##.");
+    System.out.println(ConsoleColors.YELLOW + ".##.......########..######...##.....##..##.....##....##.....##..######.....##.....##.....");
+    System.out.println(ConsoleColors.YELLOW + ".##.......##...##...##.......##.....##..##.....##....##.....##.......##....#########..##.");
+    System.out.println(ConsoleColors.YELLOW + ".##....##.##....##..##.......##.....##..##.....##....##.....##.##....##....##.....##.####");
+    System.out.println(ConsoleColors.YELLOW + "..######..##.....##.########.########..####....##.....#######...######.....##.....##..##.");
+    System.out.println();
+    System.out.println();
+		System.out.println(ConsoleColors.PURPLE + ".......##.##.....##....###....##....##..........##..#######...######..########");
+		System.out.println(ConsoleColors.PURPLE + ".......##.##.....##...##.##...###...##..........##.##.....##.##....##.##......");
+		System.out.println(ConsoleColors.PURPLE + ".......##.##.....##..##...##..####..##..........##.##.....##.##.......##......");
+		System.out.println(ConsoleColors.PURPLE + ".......##.##.....##.##.....##.##.##.##..........##.##.....##..######..######..");
+		System.out.println(ConsoleColors.PURPLE + ".##....##.##.....##.#########.##..####....##....##.##.....##.......##.##......");
+		System.out.println(ConsoleColors.PURPLE + ".##....##.##.....##.##.....##.##...###....##....##.##.....##.##....##.##......");
+		System.out.println(ConsoleColors.PURPLE + "..######...#######..##.....##.##....##.....######...#######...######..########");
+		System.out.println();
+		System.out.println();
+		System.out.println(ConsoleColors.BLUE + "....###....##.......########.......##....###....##....##.########..########...#######.");
+		System.out.println(ConsoleColors.BLUE + "...##.##...##.......##.............##...##.##...###...##.##.....##.##.....##.##.....##");
+		System.out.println(ConsoleColors.BLUE + "..##...##..##.......##.............##..##...##..####..##.##.....##.##.....##.##.....##");
+		System.out.println(ConsoleColors.BLUE + ".##.....##.##.......######.........##.##.....##.##.##.##.##.....##.########..##.....##");
+    System.out.println(ConsoleColors.BLUE + ".#########.##.......##.......##....##.#########.##..####.##.....##.##...##...##.....##");
+    System.out.println(ConsoleColors.BLUE + ".##.....##.##.......##.......##....##.##.....##.##...###.##.....##.##....##..##.....##");
+    System.out.println(ConsoleColors.BLUE + ".##.....##.########.########..######..##.....##.##....##.########..##.....##..#######.");
+    System.out.println();
+    System.out.println();
+		System.out.println(ConsoleColors.YELLOW + "Somos unos programadores principiantes, ojala disfruten del juego ;)");
 	}
-	  
-    public static void main(String[] args) {
-		int score = 0;
-		menu(score);
-    }
 }
